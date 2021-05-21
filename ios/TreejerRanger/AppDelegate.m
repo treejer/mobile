d959389357fb06173d47a00c9fc7b83cdd907341
amirhoseinhesarakii <28463719amir>
2021-05-21 20:33:54 +0430

    [add] firebase app [add] firebase and google analytics

--- ios/TreejerRanger/AppDelegate.m
+++ ios/TreejerRanger/AppDelegate.m
@@ -1,3 +1,4 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -39,6 +40,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+      [FIRApp configure];
+  }
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
