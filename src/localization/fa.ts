38ca8673c1ab5238445f2c7b6c4ad255f43199bb
Amir Habibzadeh <habibzade.amir@gmail.com>
2022-04-16 20:52:05 +0300

    New translations en.ts (Persian)

--- src/localization/fa.ts
+++ src/localization/fa.ts
@@ -1,4 +1,240 @@
-export const fa = {
-  language: 'زبان',
-  welcome: 'خوش آمدید',
+export const en = {
+  language: 'Language',
+  balance: 'Balance (DAI)',
+  plantedTrees: 'Planted Trees',
+  withdraw: 'Withdraw',
+  offlineMap: {
+    title: 'Offline Map',
+    downloadArea: 'Download this area?',
+    permissionBlocked: 'Permission Blocked',
+    downloaded: 'Map download completed',
+    isDownloaded: 'The map was already downloaded',
+    failed: 'Map download failed!',
+    delete: 'Delete',
+    savedAreas: 'Saved Areas',
+    noOfflineArea: 'No offline area found',
+    addArea: 'Add Area',
+    viewAll: 'View All',
+    download: 'Download'
+  },
+  getVerified: 'Get Verified',
+  help: 'Help',
+  logout: 'Log Out',
+  verified: 'Verified',
+  notVerified: 'Not Verified',
+  invite: {
+    title: 'Invite',
+    org: 'Organization',
+    friend: 'Friend'
+  },
+  treeInventory: {
+    title: 'Tree Inventory',
+    updateTitle: 'Updated Offline',
+    submitAll: 'Submit All',
+    showProgress: 'Show Progress'
+  },
+  treeDetails: {
+    cannotUpdate: {
+      title: 'Cannot Update',
+      details: 'Last update is pending',
+      wait: 'Please wait for {{seconds}}'
+    },
+    gpsCoords: 'GPS Coordinates',
+    funder: 'Funder',
+    notFounded: 'Not Funded Yet!',
+    lastUpdate: 'Last Update',
+    born: 'Born',
+    photo: 'Photo',
+    photos: 'Photos',
+    update: 'Update'
+  },
+  warning: 'Warning',
+  notVerifiedTree: 'Not verified tree, please wait until being verified!',
+  treeStatus: {
+    Submitted: 'Submitted',
+    Temp: 'Not Verified',
+    OfflineCreate: 'Planted Offline',
+    OfflineUpdate: 'Updated Offline'
+  },
+  cannotUpdateTree: "TreeSpec is empty, You can't update tree, please contact support.",
+  transactionFailed: {
+    title: 'Transaction Failed!',
+    tryAgain: 'Please try again, contact customer support if needed'
+  },
+  noInternet: 'No Internet',
+  submitWhenOnline: 'You can submit this tree when you have fast and stable internet connection',
+  offlineTreesSubmitted: 'All offline trees Submitted',
+  tryAgain: 'Please try again!',
+  cancel: 'Cancel',
+  error: 'Error',
+  plantFirstTree: 'Plant your first tree',
+  noOfflineTree: 'No offline {{type}} tree',
+  offlineTrees: 'Offline {{type}} Trees',
+  planted: 'planted',
+  Planted: 'Planted',
+  updated: 'updated',
+  Updated: 'Updated',
+  noAssigned: 'There is no assigned Tree',
+  submittedTrees: 'Submitted Trees',
+  notSubmittedTrees: 'Not Verified Trees',
+  noPlantedTrees: "You haven't planted any trees yet",
+  pendingVerification: 'Pending verification',
+  addWalletAddress: 'Add wallet address',
+  done: 'Done!',
+  addPhone: 'Add phone',
+  addName: 'Add name',
+  submitLocation: 'Submit location',
+  openMap: 'Open Map',
+  uploadIdCard: 'Upload ID card',
+  joiningAsOrganization: 'Joining as organization owner?',
+  verify: 'Verify',
+  resend: 'Resend',
+  sendCode: 'Send Code',
+  unknownError: 'Unknown Error. Please contact customer support!',
+  physicalLicense: 'ID card, passport or driving license',
+  errors: {
+    verificationCode: 'Code is wrong',
+    phoneNumber: 'Invalid phone number'
+  },
+  'verified!': 'Verified!',
+  greenBlock: {
+    notVerified: {
+      title: 'Please verify your account',
+      details: 'You must verify your account before joining a green block.'
+    }
+  },
+  supplyCapReached: 'You reached your supply cap limit. Please contact customer support to plant more trees!',
+  openCamera: 'Open Camera',
+  submitTree: {
+    journeyIssue: 'Photo, Location',
+    treeSpecEmpty: "TreeSpec is empty, You can't update tree, please contact support.",
+    treeDataNotLoaded: "TreeData not loaded, You can't plant tree, refresh the page and try again.",
+    noWallet: {
+      title: 'No wallet',
+      details: 'Wallet not provided'
+    },
+    updated: 'The tree has been successfully updated',
+    submitted: 'The tree has been successfully submitted',
+    error: 'Error occurred',
+    transactionFailed: "Transaction couldn't be completed",
+    confirm: 'Please confirm to plant the tree',
+    photoUpdated: 'Your photo is being uploaded',
+    updateTree: 'Update tree',
+    submitTree: 'Submit a new tree',
+    takePhoto: 'Take a photo of the tree',
+    treeLocation: 'Submit tree location',
+    updateTreeLocation: 'Update Tree Location',
+    uploadPhoto: 'Upload photo',
+    signInWallet: 'Sign with wallet',
+    singleTree: 'Single Tree',
+    nursery: 'Nursery',
+    focusedNursery: 'How many trees to plant?',
+    nurseryCount: 'Nursery ({{count}} trees)',
+    startToPlant: 'Start To Plant',
+    startToPlantNursery: 'Start To Plant {{count}} trees',
+    update: 'Update',
+    continue: 'Continue',
+    nurserySubmitted: 'Nursery successfully submitted',
+    nurserySubmitting: 'We are submitting your nursery',
+    offlineLoading: 'We are submitting your offline planted trees. This is going to take a while!\nPlease be patient',
+    offlineSubmittingNotCloseApp: "PLEASE DON'T CLOSE THE APPLICATION",
+    hashCopied: 'Hash copied to clipboard',
+    minimize: 'Minimize'
+  },
+  success: 'Success',
+  failure: 'Failure',
+  createWallet: {
+    title: 'Create Wallet',
+    setup: 'Set up wallet',
+    recoveryPhrase: 'Recovery phrase',
+    ethConnect: 'Please connect \n your Ethereum wallet!',
+    connectToMagic: 'Connect Wallet',
+    loginWithPhone: 'Log in / Sign up',
+    or: 'OR',
+    connect: 'Connect Wallet',
+    why: {
+      title: 'Why do I need that?',
+      details: 'By connecting your Ethereum wallet, you will receive your rewards faster and safer',
+      learnMore: 'Learn More'
+    },
+    failed: {
+      title: 'Failed to login',
+      details: 'Failed to authenticate via Torus. Please try again later.'
+    },
+    setupAccount: 'Set up account'
+  },
+  myProfile: {
+    attention: 'Attention!',
+    looseTree: 'You have {{treesLength}} {{treeText}} planted in offline mode and {{treeThereText}} waiting to submit, if you logout now, you will loose your {{treeText}}!',
+    logoutAndLoose: 'Logout & Loose',
+    withdraw: {
+      success: 'You successfully withdrew!'
+    },
+    lessBalance: 'Your balance is less than {{amount}}!',
+    accuracy: 'Accuracy',
+    offlineTreeAdd: 'We are detected that you are offline.\nPlease submit this tree when you have fast and stable internet connection',
+    offlineNurseryAdd: 'We are detected that you are offline.\nPlease submit this nursery when you have fast and stable internet connection',
+    copied: 'Copied to clipboard!'
+  },
+  sthWrong: 'Something went wrong!',
+  verifyPending: {
+    title: 'You’ll be verified soon!',
+    tnx: 'Thank you!',
+    verifying: 'You will receive a SMS when your identity is verified.',
+    continue: 'Continue'
+  },
+  onBoarding: {
+    first: {
+      title: 'Connect to your Wallet',
+      details: 'The new member will receive a SMS to join the Green Block in a few minutes.'
+    },
+    second: {
+      title: 'Plant Trees or Support them',
+      details: 'The new member will receive a SMS to join the Green Block in a few minutes.'
+    },
+    third: {
+      title: 'Get Rewards',
+      details: 'The new member will receive a SMS to join the Green Block in a few minutes.'
+    }
+  },
+  netInfo: {
+    error: 'Network Error!',
+    details: 'You have no internet connection! \nYou can continue planting trees when you are offline'
+  },
+  loginFailed: {
+    title: 'Login Failed',
+    message: 'Please try again, if you still have trouble, contact customer support!'
+  },
+  permission: {
+    denied: 'Permission Denied!',
+    required: 'Permissions required',
+    camera: 'Camera permission is required'
+  },
+  ok: 'OK',
+  next: 'NEXT',
+  submit: 'Submit',
+  confirm: 'Confirm',
+  loading: {
+    ranger: "Ranger's App",
+    by: 'by Treejer'
+  },
+  email: 'Email',
+  phoneNumber: 'Phone Number',
+  settings: {
+    title: 'Settings',
+    useGSN: 'Transaction Fee Sponsorship',
+    gsnDetails: 'Treejer sponsors your transaction fees. If your transactions are being failed, you can switch off sponsorship. Please remember you need MATIC token in your wallet send transactions. You may ask your rural ambassador for more help.',
+    ethBalance: 'Eth Balance:',
+    maticBalance: 'Matic Balance:'
+  },
+  networks: {
+    switchHeader: 'Network',
+    selectNetwork: 'Select a network:',
+    areYouSure: 'Are you SURE?',
+    yes: 'Yes',
+    no: 'No',
+    testTitle: 'Test Networks',
+    mainTitle: 'Main Networks',
+    restartApp: 'The application will be restart and automatically log you out'
+  }
 };
