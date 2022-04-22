667cf4d66a5b3f6690b70df21709d366ed674404
armin-bkh <arminbkh0921@yahoo.com>
2022-04-22 23:00:24 +0430

    push to store in OfflineMap

--- src/screens/Profile/screens/OfflineMap/OfflineMap.web.tsx
+++ src/screens/Profile/screens/OfflineMap/OfflineMap.web.tsx
@@ -1,22 +1,24 @@
 import Button from 'components/Button';
-import React, {useMemo} from 'react';
+import React, {useCallback} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, Linking} from 'react-native';
 import globalStyles from 'constants/styles';
 import {colors} from 'constants/values';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ChevronLeft} from 'components/Icons';
+import {useBrowserPlatform} from '../../../../utilities/hooks/useBrowserPlatform';
 
 export default function OfflineMap({navigation}) {
   const {t} = useTranslation();
+  const browserPlatform = useBrowserPlatform();
 
-  const isIos = useMemo(() => /webOS|iPhone|iPad|iPod/i.test(navigator.userAgent), []);
-  const href = useMemo(() => {
-    let linkHref = 'https://play.google.com';
-    if (isIos) {
-      linkHref = 'https://apple.com';
+  const pushToStoreHandler = useCallback(() => {
+    let href = 'https://play.google.com/store/apps/details?id=com.treejer.ranger';
+    if (browserPlatform === 'iOS') {
+      href = 'https://apple.com';
     }
-    return linkHref;
+
+    Linking.openURL(href);
   }, []);
 
   return (
@@ -36,9 +38,7 @@ export default function OfflineMap({navigation}) {
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{marginBottom: 10}}>{t('offlineMap.notSupported')}</Text>
 
-        <a href={href} target="_blank" rel="nofollow noreferrer">
-          <Button caption={t('offlineMap.downloadApplication')} />
-        </a>
+        <Button caption={t('offlineMap.downloadApplication')} onPress={pushToStoreHandler} />
       </View>
     </SafeAreaView>
   );
