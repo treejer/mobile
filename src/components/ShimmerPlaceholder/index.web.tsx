6b76a2f1a8492c966e7309c2d3e6974d262ee9fa
armin-bkh <arminbkh0921@yahoo.com>
2022-04-23 18:11:24 +0430

    ShimmerPlaceHolder props type

--- src/components/ShimmerPlaceholder/index.web.tsx
+++ src/components/ShimmerPlaceholder/index.web.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import Skeleton from 'react-loading-skeleton';
 
-export default function ShimmerPlaceHolder({style}) {
+interface ShimmerPlaceHolderPropsType {
+  style: React.CSSProperties;
+}
+
+export default function ShimmerPlaceHolder({style}: ShimmerPlaceHolderPropsType) {
   return (
     <View style={{alignItems: 'center', justifyContent: 'center'}}>
       <Skeleton style={style} />
