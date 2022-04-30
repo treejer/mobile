c532c3b28bfb814808b390699785cf01162d56e9
Hamed Navabian <hamed.navabian@gmail.com>
2022-04-30 08:13:18 +0430

    v1.2.1-rc + reduce images size

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -2,6 +2,5 @@
 import MaticLogo from './matic-network-logo-vector.png';
 import RinkebyLogo from './rinkeby-logo.jpeg';
 import TreejerIcon from './icon.png';
-import TreejerSplash from './splash.png';
 
-export {MaticLogo, RinkebyLogo, TreejerIcon, TreejerSplash};
+export {MaticLogo, RinkebyLogo, TreejerIcon};
