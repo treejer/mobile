cea8c82fb0ae97a6ec0169a72d84b4675d4ddf54
Hamed Navabian <hamed.navabian@gmail.com>
2022-12-07 19:58:34 +0330

    biconomy is working

--- src/utilities/helpers/sendTransaction.ts
+++ src/utilities/helpers/sendTransaction.ts
@@ -1,8 +1,8 @@
 import {TransactionReceipt} from 'web3-core';
-import {Biconomy} from '@biconomy/mexa';
 
 import Web3, {Magic} from 'services/Magic';
 import {ContractType, NetworkConfig} from 'services/config';
+import {transactionBiconomy} from 'services/Biconomy';
 
 export async function sendWeb3Transaction(
   magic: Magic,
@@ -21,58 +21,21 @@ export async function sendWeb3Transaction(
   console.log('1 - Is main net?', config.isMainnet);
   console.log(method, '<===== method');
 
-  const _config = {
-    apiKey,
-    contractAddress: config.biconomyAddress,
-    debug: true,
-    // auditorsCount: config.isMainnet ? 1 : 0,
-    // methodSuffix: '_v4',
-    // jsonStringifyRequest: true,
-
-    // performDryRunViewRelayCall: false,
-    // preferredRelays: [config.preferredRelays],
-    // relayLookupWindowBlocks: Number(config.relayLookupWindowBlocks),
-    // relayRegistrationLookupBlocks: Number(config.relayRegistrationLookupBlocks),
-    // pastEventsQueryMaxPageSize: Number(config.pastEventsQueryMaxPageSize),
-
-    // preferredRelays: ['https://rinkeby-gsn-relayer.treejer.com'],
-    // preferredRelays: ['https://ropsten-gsn-relayer.treejer.com/gsn1/getaddr'],
-  };
-
   if (!useBiconomy) {
     return sendTransactionWithoutBiconomy(config, contractType, web3, wallet, args, method);
   }
 
-  // @ts-ignore
-  const biconomy = new Biconomy(magic.rpcProvider, _config);
+  const biconomy = await transactionBiconomy({apiKey, magic});
+  const web3Biconomy = new Web3(biconomy as any);
+  const ethContract = new web3Biconomy.eth.Contract(contract.abi as any, contract.address);
 
   console.log('2 - Relay provider created', config.isMainnet);
 
-  // gsnProvider.addAccount(wallet);
-  console.log('3 - Account linked to the relay provider', config.isMainnet);
-
-  const gas = await web3.eth.estimateGas({from: wallet});
-
-  console.log('Gas estimated', gas);
-
-  const gasPrice = await web3.eth.getGasPrice();
-
-  const web3Biconomy = new Web3(biconomy as any);
-
-  const ethContract = new web3Biconomy.eth.Contract(contract.abi as any, contract.address);
-
-  console.log('4 - Started sending the transaction', config.isMainnet);
-
-  // * Sends the transaction via the biconomy
-
-  console.log(method, 'Method', args, 'args', wallet, 'address');
   return ethContract.methods[method](...args).send('eth_sendTransaction', {
     from: wallet,
-    // gas: 1e6,
-    // gasPrice,
     signatureType: 'PERSONAL_SIGN',
     domainName: 'Powered by Biconomy',
-  }) as TransactionReceipt;
+  });
 }
 
 export async function sendTransactionWithoutBiconomy(
